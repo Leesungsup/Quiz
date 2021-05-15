9b46df3564c5b78d1ca324bc88af3af71a82dcd3
Leesungsup <tmdtjq0116@naver.com>
2021-05-15 22:43:57 +0900

    padding message

--- quiz_app_test/lib/screen/screen_home.dart
+++ quiz_app_test/lib/screen/screen_home.dart
@@ -10,6 +10,16 @@ class _HomeScreenState extends State<HomeScreen>{
     Size screenSize = MediaQuery.of(context).size;
     double width=screenSize.width;
     double height=screenSize.height;
-    return SafeArea(child: Scaffold(appBar:AppBar(title:Text('My Quiz'),backgroundColor: Colors.deepPurple, leading:Container(),),body:Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center,children:<Widget>[Center(child:Image.asset('images/quiz.png',width:width*0.8,height:height*0.8))])));
+    return SafeArea(
+      child: Scaffold(
+        appBar:AppBar(
+          title:Text('My Quiz'),
+          backgroundColor: Colors.deepPurple, leading:Container(),),
+          body:Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center,
+          children:<Widget>[Center(child:Image.asset('images/quiz.png',width:width*0.8,height:height*0.8),),
+          Padding(padding:EdgeInsets.all(width*0.024),),
+          Text('flutter quiz',style: TextStyle(fontSize: width*0.065,fontWeight: FontWeight.bold,),),
+          Text('퀴즈를 풀기 전 안내사항입니다\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',textAlign:TextAlign.center,),
+          Padding(padding:EdgeInsets.all(width*0.048),)])));
   }
 }
