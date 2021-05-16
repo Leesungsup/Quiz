0b8fddb0cfe3b8e54a6351c6a0d7c64f0a754b9a
Leesungsup <tmdtjq0116@naver.com>
2021-05-16 18:56:33 +0900

    swiper active

--- quiz_app_test/lib/screen/screen_quiz.dart
+++ quiz_app_test/lib/screen/screen_quiz.dart
@@ -13,6 +13,7 @@ class _QuizScreenState extends State<QuizScreen>{
   List<int> _answers = [-1,-1,-1];
   List<bool> _answerState = [false,false,false,false];
   int _currentIndex=0;
+  SwiperController _controller = SwiperController();
   Widget build(BuildContext context){
     Size screenSize = MediaQuery.of(context).size;
     double width=screenSize.width;
@@ -27,7 +28,9 @@ class _QuizScreenState extends State<QuizScreen>{
             ),
             width:width*0.85, 
             height:height*0.5,
-            child:Swiper(physics:NeverScrollableScrollPhysics(),
+            child:Swiper(
+              controller:_controller,
+              physics:NeverScrollableScrollPhysics(),
             loop:false,
             itemCount:widget.quizs.length,
             itemBuilder:(BuildContext context,int index){
@@ -84,6 +87,7 @@ class _QuizScreenState extends State<QuizScreen>{
             else{
               _answerState=[false,false,false,false];
               _currentIndex+=1;
+              _controller.next();
             }
           })
           )
