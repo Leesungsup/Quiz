d8babf759818b24a0135777432f79e05a2c88c89
Leesungsup <tmdtjq0116@naver.com>
2021-05-17 18:45:23 +0900

    quiz result text

--- quiz_app_test/lib/screen/screen_result.dart
+++ quiz_app_test/lib/screen/screen_result.dart
@@ -25,10 +25,34 @@ class ResultScreen extends StatelessWidget{
         color:Colors.deepPurple,),
         width:width*0.85,
         height:height*0.5,
-        child:Column(children: <Widget>[],)
-        )
-        )
-        )
+        child:Column(children: <Widget>[
+          Padding(padding:EdgeInsets.only(bottom:width*0.048),
+          ),
+          Container(decoration:BoxDecoration(borderRadius: BorderRadius.circular(20),
+          border: Border.all(color:Colors.deepPurple),
+          color:Colors.white,),
+          width:width*0.73,
+          height:height*0.25,
+          child:Column(children: <Widget>[
+          Container(
+            padding:EdgeInsets.fromLTRB(0, width*0.048, 0, width*0.012),
+            child:Text('수고하셨습니다!',
+            style:TextStyle(fontSize: width*0.055,
+            fontWeight:FontWeight.bold,),
+            ),
+            ),
+            Text('당신의 점수는',style:TextStyle(fontSize: width*0.048,
+            fontWeight:FontWeight.bold,),
+            ),
+            Expanded(child: Container(),)
+          ],
+          ),
+          ),
+        ],
+        ),
+        ),
+        ),
+        ),
     );
   }
 }
