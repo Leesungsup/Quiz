0fd9029d7a5a845a36e7dfb7cd7665e9aec03f09
Leesungsup <tmdtjq0116@naver.com>
2021-06-26 23:41:49 +0900

    admin

---
+++ quiz_app_test/lib/screen/screen_home copy.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_test/model/api_adapter.dart';
+import 'package:quiz_app_test/model/model_quiz.dart';
+import 'package:quiz_app_test/screen/screen_quiz.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+class HomeScreen extends StatefulWidget{
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  List<Quiz> quizs=[];
+  bool isLoading = false;
+  String url="https://lit-hamlet-76910.herokuapp.com/quiz/PlayerQuiz/5/";
+  _fetchQuizs() async{
+    setState((){
+      isLoading = true;
+    });
+    final response =await http.get(Uri.parse(url));
+    if(response.statusCode==200){
+      setState((){
+        quizs=parseQuizs(utf8.decode(response.bodyBytes));
+        isLoading=false;
+      });
+    }
+    else{
+      throw Exception('failed to load data');
+    }
+  }
+ /* List<Quiz> quizs=[
+    Quiz.fromMap({
+      'title':'test',
+      'candidates':['a','b','c','d'],
+      'answer':0
+    }),
+    Quiz.fromMap({
+      'title':'test',
+      'candidates':['a','b','c','d'],
+      'answer':0
+    }),
+    Quiz.fromMap({
+      'title':'test',
+      'candidates':['a','b','c','d'],
+      'answer':0
+    }),
+  ];*/
+  @override
+  Widget build(BuildContext context){
+    Size screenSize = MediaQuery.of(context).size;
+    double width=screenSize.width;
+    double height=screenSize.height;
+    return WillPopScope(onWillPop:() async=>false,
+    child: SafeArea(
+      child: Scaffold(
+        key:_scaffoldKey,
+        appBar:AppBar(
+          title:Text('Football Player Quiz'),
+          backgroundColor: Colors.deepPurple, leading:Container(),),
+          body:
+          Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center,
+          children:<Widget>[Center(child:Image.asset('images/quiz.png',width:width*0.8,height:height*0.5),),
+          //Padding(padding:EdgeInsets.all(width*0.001),),
+          Text('flutter quiz',style: TextStyle(fontSize: width*0.065,fontWeight: FontWeight.bold,),),
+          Text('이 퀴즈는 축구선수 인지도 조사\n퀴즈 입니다.',textAlign:TextAlign.center,),
+          //Padding(padding:EdgeInsets.all(width*0.5),),
+          _buildStep(width,'1.랜덤으로 나오는 퀴즈 5개를 풀어보세요.'),
+          _buildStep(width,'2.랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+          _buildStep(width,'3.랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+          Padding(padding:EdgeInsets.all(width*0.024),),
+          Container(padding: EdgeInsets.only(bottom:width*width*0.036),
+          child : Center(
+            child: ButtonTheme(
+              minWidth:width*0.8,
+              height:height*0.05,
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),
+              ),
+              child: RaisedButton(
+                child:Text(
+                  'Quiz start',
+                  style:TextStyle(color:Colors.white),
+                  ),
+                  color:Colors.deepPurple,
+                  onPressed:(){
+                    _scaffoldKey.currentState.showSnackBar(SnackBar(
+                      content:Row(
+                        children:<Widget>[
+                          CircularProgressIndicator(),
+                          Text('Loading....'),
+                        ])));
+                    _fetchQuizs().whenComplete((){
+                      return Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context)=>QuizScreen(quizs:quizs,
+                        ),
+                        )
+                        );
+                    });
+                    
+                  },
+                  )
+            ),
+            ))
+          ]
+          )
+          )
+          )
+          );
+  }
+  Widget _buildStep(double width,String title){
+  return Container(
+    padding: EdgeInsets.fromLTRB(
+      width*0.048,
+      width*0.024,
+      width*0.048,
+      width*0.024),
+      child:Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children : <Widget>[
+          Icon(
+            Icons.check_box,
+            size:width*0.04,
+            ),
+          Padding(
+            padding: EdgeInsets.only(right:width*0.024),
+            ),
+            Text(title),
+          ],
+        ),
+    );
+  }
+}
